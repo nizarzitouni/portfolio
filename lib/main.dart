c8a2b9d42dcdbb5c7192ee782067e9af3478d3a5
Nizar <nizarzayto@gmail.com>
2024-02-15 13:22:55 +0100

    V8 Aded analyitics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nizar_ztn_portfolio/firebase_options.dart';
 
 import 'app/portfolio_app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PortfolioApp());
 }
